c6bd3e0bbe0d4efdfe26b55c4eb84a97eea6051a
Jamie Lopez <jamie555x@gmail.com>
2022-04-14 11:36:45 -0700

    some other stuff

--- BlazorWebApp/wwwroot/scripts.js
+++ BlazorWebApp/wwwroot/scripts.js
@@ -1,8 +1,18 @@
-import "../Other/Example.js";
+require(["esri/config", "esri/Map", "esri/views/MapView"], function (esriConfig, Map, MapView) {
+    esriConfig.apiKey = "AAPK67d587eafb5c46c4aa6ffd3cd40bdb17tkAAPxj614aHsv67NfDbx1I77lsiBa5pSGUvYOvPqa2rr9OOE8T2PcYreAVrSTe-";
 
-/*export function getPromptInstance(): Prompt {
-    return new Prompt();
-}*/
+    const map = new Map({
+        basemap: "arcgis-topographic" // Basemap layer service
+    });
+
+    const view = new MapView({
+        map: map,
+        center: [-118.805, 34.027], // Longitude, latitude
+        zoom: 13, // Zoom level
+        container: "viewDiv" // Div element
+    });
+});
+
 
 export function showPrompt(message) {
     console.log("show prompt");
